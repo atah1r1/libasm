ff6374e23507927325cad78dfdc41ecc3c68a089
Amine Tahiri <atahiri@student.1337.ma>
2021-03-05 11:31:12 +0100

    adding other comments

--- ft_read.s
+++ ft_read.s
@@ -3,13 +3,13 @@ section.text:
     extern  ___error
 
 _ft_read:
-	mov rax, 0x2000003 ; 0x2000000 (MacOS ?)
-	syscall ; 64 bits version of int 0x80
+	mov rax, 0x2000003 											; 0x2000000 (MacOS ?)
+	syscall 													; 64 bits version of int 0x80
         jc _exit_error
 	ret
 _exit_error:
-    push rax    ;save errno
-	call ___error   ;rax is now points to external variable errno.
-	pop qword[rax]  ;The QWORD PTR is just a size specifier (It means that a 64 bit value is read from the address)
+    push rax   												 	;save errno
+	call ___error   											;rax is now points to external variable errno.
+	pop qword[rax]  											;The QWORD PTR is just a size specifier (It means that a 64 bit value is read from the address)
     mov rax, -1
     ret
